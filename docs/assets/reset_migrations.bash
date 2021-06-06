24eb13e221158753cf40de8424bcc038bdc7303a
Andres Garcia <joseandresgarciafernandez@dev.azure.com>
2021-06-06 16:42:24 -0600

    Updated to the .gitpod.yml

--- docs/assets/reset_migrations.bash
+++ docs/assets/reset_migrations.bash
@@ -1,7 +1,7 @@
 rm -R -f ./migrations &&
 pipenv run init &&
-psql -U gitpod -c 'DROP DATABASE example;' || true &&
-psql -U gitpod -c 'CREATE DATABASE example;' &&
-psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d example &&
+psql -U gitpod -c 'DROP DATABASE ODS;' || true &&
+psql -U gitpod -c 'CREATE DATABASE ODS;' &&
+psql -U gitpod -c 'CREATE EXTENSION unaccent;' -d ODS &&
 pipenv run migrate &&
 pipenv run upgrade
